6358f7999930823950aa682f0cf6d2ec96cca6c9
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-17 15:00:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        debugger;
+        messages.push(message)
+
+    }
+    return messages;
+}
+
+
+function countDown(num) {
+    let i = num;
+    while (0 <= i) {
+        console.log(i);
+        --i;
+        debugger;
+    }
+}
